6a27dd0c1857911f0fb32de366f8e8204422096b
villiger9 <ali.shaker93@gmail.com>
2025-12-29 14:42:24 +0300

    added help layout and components

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,31 @@ import {
 //pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Faq from "./pages/help/faq";
+import Contact from "./pages/help/Contact";
 
 //layouts
 import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Route>
   )
 );
+
 function App() {
   return <RouterProvider router={router} />;
 }
 
 // Switch was replaced with Routes in new react-router-dom
+// createBrowserRouter instead of browserRouter
 
 export default App;
---
+++ src/layouts/HelpLayout.jsx
@@ -0,0 +1,20 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function HelpLayout() {
+  return (
+    <div className="help-layout">
+      <h2>Website Help</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+        excepturi!
+      </p>
+
+      <nav>
+        <NavLink to="faq">View the FAQ</NavLink>
+        <NavLink to="contact">Contact Us</NavLink>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
+}
